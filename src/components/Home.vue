23b2c377676a5da3e88df9028215c9c00a9fc73c
nbittich <contact@bittich.be>
2017-10-29 21:15:58 +0100

    pain prepare

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+    <div class="jumbotron">
+      <h1 class="display-3">{{ msg }}</h1>
+      <p class="lead">{{ content }}</p>
+    </div>
 </template>
 
 <script>
@@ -9,7 +10,8 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'wesh hamo'
+      msg: 'HealthStats - Introduction',
+      content: 'Cette application a pour objectif d\'enregistrer votre état de santé au cours du temps(maladie, insomnies, douleurs chroniques...), d\'élaborer des statistiques sur base de ces données et de générer des rapports que vous pourrez présenter à votre médecin à vos prochaines visites.'
     }
   }
 }
