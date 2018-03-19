711a1d957e808547043dbe0874ec737aa63455c9
Nordine Bittich <nbittich@csc.com>
2018-03-19 05:48:03 +0100

    store

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ const store = new Vuex.Store({
     news: {
       page: {
         number: 0,
-        size: 2,
+        size: 4,
         first: true,
         last: false,
         totalPages: 1,
